36e7aadcda81f778ee122aec1cdc1a8498eecfdb
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-08-10 01:58:30 +0300

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main: prints the name of the program
+ * argc: number of arguments
+ * argv: array of arguments
+ * return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
