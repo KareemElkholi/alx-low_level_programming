0a99a09af5432c031bd1d861319aedcea30e9fc2
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-08-10 03:12:43 +0300

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,17 @@
 
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int mul = num1 * num2;
+	int num1, num2, mul;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
 
 	printf("%d\n", mul);
 	return (0);
