142b4160769c5472abca1f3185375454e3059b11
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-08-10 05:08:45 +0300

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the result of sum
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+		{
+			if (*c < '0' || *c > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+		}
+		sum = sum + atoi(argv[argc]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
