e71d51890a7bb23753ae6071ac300710558b3260
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-08-18 03:29:25 +0300

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int i;
+	char str[] = "0123456789\n";
+
+	for (i = 0; i < 11; i++)
+		_putchar([i]);
+}
