b07886c0f85fdbbc471c556bd8317bbaa2642f09
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-08-18 03:34:03 +0300

    4. I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+	char str[] = "01356789\n";
+
+	for (i = 0; i < 9; i++)
+		_putchar(str[i]);
+}
