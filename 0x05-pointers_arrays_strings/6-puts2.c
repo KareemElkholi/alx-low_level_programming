91cb3fe0b358fb47295dbb7a0d9a99caa570ec26
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-08-21 08:44:36 +0300

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string
+ */
+
+puts2(char *str)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+		i++
+	}
+}
