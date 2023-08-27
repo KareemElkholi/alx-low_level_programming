d0301f1215b579de770113d08beb919211d2b056
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-08-27 04:35:15 +0300

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalizes all words of a string
+ * @s: string
+ * Return: result
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char c[14] = " \t\n,:.!?\"(){}";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 14; j++)
+		{
+			if (s[i] == c[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
