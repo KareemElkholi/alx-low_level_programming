82f60bbd6d72d931caf47820be7dad733c5b9118
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-08-27 15:01:00 +0300

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *rot13 - encodes a string using rot13
+ * @s: string
+ * Return: result
+ */
+
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+			s[i] += 13;
+		else if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+			s[i] -= 13;
+	}
+	return (s);
+}
