377849daaf3f04eb199b1d6bbd2f883c3243d374
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-08-27 15:36:33 +0300

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,14 +9,14 @@
 char *cap_string(char *s)
 {
 	int i, j;
-	char c[14] = " \t\n,:.!?\"(){}";
+	char c[] = " \t\n,:.!?\"(){}";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 			s[i] -= 32;
 
-		for (j = 0; j < 13; j++)
+		for (j = 0; c[j] != '\0'; j++)
 		{
 			if (s[i] == c[j])
 			{
