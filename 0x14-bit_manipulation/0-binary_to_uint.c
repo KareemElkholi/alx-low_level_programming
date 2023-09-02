9efff9144048d02c265b32536ab1eac1f6ae9afc
KareemElkholi <kareemshrf@std.mans.edu.eg>
2023-09-02 04:53:48 +0300

    .

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,45 +1,18 @@
 #include "main.h"
 
-/**
- * _pow_recursion - returns the value of x raised to the power of y
- * @x: number
- * @y: number
- * Return: result
- */
-
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	else if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, --y));
-}
-
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: pointing to a string of 0 and 1 chars
- * Return: result
- */
-
 unsigned int binary_to_uint(const char *b)
 {
+	int i;
 	unsigned int sum = 0;
-	int i, length;
-	
-	if (b == NULL)
+
+	if (!b)
 		return (0);
 
-	for (length = 0; b[length] != '\0'; length++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[length] != '0' && b[length] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
+		sum = 2 * sum + (b[i] - '0');
 	}
-
-	for (i = 0; i < length; i++)
-	{
-		sum += (b[i] - '0') * _pow_recursion(2, (length - i - 1));
-	}
-
 	return (sum);
 }
